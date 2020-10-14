0f34090e7585fdba5b957deca8d4e6895a192a0b
Alexandre Piveteau <alexandre@piveteau.email>
2020-10-14 18:01:26 +0200

    Add some sample Jetpack Compose code

--- app/src/main/java/ch/heigvd/ihm/stickies/MainActivity.kt
+++ app/src/main/java/ch/heigvd/ihm/stickies/MainActivity.kt
@@ -2,14 +2,14 @@ package ch.heigvd.ihm.stickies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.Text
 import androidx.compose.ui.platform.setContent
+import ch.heigvd.ihm.stickies.stickies.Demo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world.")
+            Demo()
         }
     }
 }
---
+++ app/src/main/java/ch/heigvd/ihm/stickies/stickies/Sticky.kt
@@ -0,0 +1,56 @@
+package ch.heigvd.ihm.stickies.stickies
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Stack
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+
+@Composable
+fun Sticky(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.surface,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Surface(
+        modifier = modifier.preferredSize(256.dp)
+            .clickable(onClick = onClick),
+        color = color,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+    ) {
+        Stack(alignment = Alignment.Center) {
+            content()
+            val (checked, setChecked) = remember { mutableStateOf(false) }
+            Switch(checked = checked, onCheckedChange = { setChecked(!checked) })
+        }
+    }
+}
+
+@Composable
+@Preview
+fun Demo() {
+    Stack(Modifier.fillMaxSize(), alignment = Alignment.Center) {
+        val (value, setValue) = remember { mutableStateOf(Color.Red) }
+        Sticky(
+            color = value,
+            onClick = { setValue(Color.Black) }
+        ) {
+            Text("Salut", textAlign = TextAlign.Center)
+        }
+    }
+}

021a912da7e37fda2973463f01de0b30bbe23404
Alexandre Piveteau <alexandre@piveteau.email>
2020-10-14 17:29:20 +0200

    Set Jetpack Compose up

--- app/src/main/java/ch/heigvd/ihm/stickies/MainActivity.kt
+++ app/src/main/java/ch/heigvd/ihm/stickies/MainActivity.kt
@@ -2,10 +2,14 @@ package ch.heigvd.ihm.stickies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello world.")
+        }
     }
 }

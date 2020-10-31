cc00849823529e5a4cec9de39554ac9f5f084556
Alexandre Piveteau <alexandre@piveteau.email>
2020-11-01 00:05:41 +0100

    Update Jetpack Compose version

--- app/src/main/java/ch/heigvd/ihm/stickies/stickies/Sticky.kt
+++ app/src/main/java/ch/heigvd/ihm/stickies/stickies/Sticky.kt
@@ -1,17 +1,18 @@
 package ch.heigvd.ihm.stickies.stickies
 
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedTask
+import androidx.compose.runtime.compositionReference
+import androidx.compose.runtime.onActive
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,18 +27,47 @@ fun Sticky(
     onClick: () -> Unit = {},
     content: @Composable () -> Unit
 ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text("Début", Modifier.align(Alignment.Start))
+        Text("Fin", Modifier.align(Alignment.End))
+    }
+    onActive {
+
+    }
     Surface(
-        modifier = modifier.preferredSize(256.dp)
+        modifier = modifier
+            .preferredSize(256.dp)
             .clickable(onClick = onClick),
         color = color,
         shape = RoundedCornerShape(8.dp),
         elevation = 4.dp,
     ) {
-        Box(alignment = Alignment.Center) {
+        Box(
+            Modifier.padding(8.dp),
+            alignment = Alignment.Center
+        ) {
             content()
-            val (checked, setChecked) = remember { mutableStateOf(false) }
-            Switch(checked = checked, onCheckedChange = { setChecked(!checked) })
         }
+    }
+}
+
+@Composable
+@Preview
+fun StickyBubble() {
+    Box(
+        Modifier.background(Color.White, CircleShape)
+            .border(8.dp, Color.Red, CircleShape)
+            .size(56.dp),
+        alignment = Alignment.Center,
+    ) {
+        Text(
+            "!",
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
@@ -45,10 +75,8 @@ fun Sticky(
 @Preview
 fun Demo() {
     Box(Modifier.fillMaxSize(), alignment = Alignment.Center) {
-        val (value, setValue) = remember { mutableStateOf(Color.Red) }
         Sticky(
-            color = value,
-            onClick = { setValue(Color.Black) }
+            onClick = { }
         ) {
             Text("Salut", textAlign = TextAlign.Center)
         }

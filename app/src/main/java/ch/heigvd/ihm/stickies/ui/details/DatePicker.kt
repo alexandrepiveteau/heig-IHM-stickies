90f8349bbcdec9e198736fcb6def50d3aa3343fd
Alexandre Piveteau <alexandre@piveteau.email>
2020-11-01 18:34:19 +0100

    Refactor DatePicker
    Co-authored-by: Matthieu Burguburu <matthieu.burguburu@gmail.com>

--- app/src/main/java/ch/heigvd/ihm/stickies/ui/details/DatePicker.kt
+++ app/src/main/java/ch/heigvd/ihm/stickies/ui/details/DatePicker.kt
@@ -4,110 +4,70 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ripple.RippleIndication
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import java.util.*
 
-enum class SelectionDate {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
+enum class SelectionDate(val title: String) {
+    Monday("Mon"),
+    Tuesday("Tue"),
+    Wednesday("Wed"),
+    Thursday("Thu"),
+    Friday("Fri"),
+    Saturday("Sat"),
+    Sunday("Sun");
 }
 
 @Composable
 fun DatePicker(
-        selected: EnumMap<SelectionDate, Boolean>,
-        onClick: (EnumMap<SelectionDate, Boolean>) -> EnumMap<SelectionDate, Boolean>,
-        modifier: Modifier = Modifier,
+    selected: Set<SelectionDate>,
+    onClick: (SelectionDate) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Row(modifier) {
-        CircularPill(
-            color = Color(0xFFF2F2F2),
-            Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Monday] = !oldEnumMap[SelectionDate.Monday] }) }, indication = null),
-            filled = selected[SelectionDate.Monday] ?: false,
-            content = {
-                Text("Mon", style = MaterialTheme.typography.subtitle1)
-                Text("02", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
+        for (day in SelectionDate.values()) {
+            CircularPill(
                 color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Tuesday] = !oldEnumMap[SelectionDate.Tuesday] }) }, indication = null),
-                filled = selected[SelectionDate.Tuesday] ?: false,
+                Modifier
+                    .clip(CircleShape)
+                    .clickable(
+                        onClick = { onClick(day) },
+                        indication = RippleIndication(),
+                    ),
+                filled = selected.contains(day),
                 content = {
-                Text("Tue", style = MaterialTheme.typography.subtitle1)
-                Text("03", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
-                color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Wednesday] = !oldEnumMap[SelectionDate.Wednesday] }) }, indication = null),
-                filled = selected[SelectionDate.Wednesday] ?: false,
-                content = {
-                Text("Wed", style = MaterialTheme.typography.subtitle1)
-                Text("04", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
-                color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Thursday] = !oldEnumMap[SelectionDate.Thursday] }) }, indication = null),
-                filled = selected[SelectionDate.Thursday] ?: false,
-                content = {
-                Text("Thu", style = MaterialTheme.typography.subtitle1)
-                Text("05", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
-                color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Friday] = !oldEnumMap[SelectionDate.Friday] }) }, indication = null),
-                filled = selected[SelectionDate.Friday] ?: false,
-                content = {
-                Text("Fri", style = MaterialTheme.typography.subtitle1)
-                Text("06", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
-                color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Saturday] = !oldEnumMap[SelectionDate.Saturday] }) }, indication = null),
-                filled = selected[SelectionDate.Saturday] ?: false,
-                content = {
-                Text("Sat", style = MaterialTheme.typography.subtitle1)
-                Text("07", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
-        CircularPill(
-                color = Color(0xFFF2F2F2),
-                Modifier.clickable(onClick = { onClick( { oldEnumMap -> oldEnumMap[SelectionDate.Sunday] = !oldEnumMap[SelectionDate.Sunday] }) }, indication = null),
-                filled = selected[SelectionDate.Sunday] ?: false,
-                content = {
-                Text("Sun", style = MaterialTheme.typography.subtitle1)
-                Text(".", style = MaterialTheme.typography.subtitle2)
-            })
-        Spacer(Modifier.width(16.dp))
+                    Text(day.title, style = MaterialTheme.typography.subtitle1)
+                    Text("02", style = MaterialTheme.typography.subtitle2)
+                })
+            Spacer(Modifier.width(16.dp))
+        }
     }
 }
 
 @Composable
+@Preview
 private fun DatePickerPreview() {
     Stack(Modifier.background(Color.White).padding(16.dp)) {
-        val (dates, setDates) = remember {
-            mutableStateOf(
-                    SelectionDate.values()
-                            .zip(MutableList(7) { false })
-                            .toMap(EnumMap(SelectionDate::class.java))
-            )
-        }
-        DatePicker(selected = dates, onClick = {  setDates -> setDates }, Modifier.align(Alignment.Center))
+        val (dates, setDates) = remember { mutableStateOf(emptySet<SelectionDate>()) }
+        DatePicker(
+            selected = dates,
+            onClick = { date ->
+                if (dates.contains(date)) {
+                    setDates(dates - date)
+                } else {
+                    setDates(dates + date)
+                }
+            },
+            Modifier.align(Alignment.Center)
+        )
     }
 }

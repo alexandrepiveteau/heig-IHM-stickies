9fcdb6356d407afeac9a7b9615de1989c26c1658
matt989253 <matthieu.burguburu@gmail.com>
2020-11-05 00:26:27 +0100

    Add some ugly numbers

--- app/src/main/java/ch/heigvd/ihm/stickies/ui/details/TimePicker.kt
+++ app/src/main/java/ch/heigvd/ihm/stickies/ui/details/TimePicker.kt
@@ -1,5 +1,6 @@
 package ch.heigvd.ihm.stickies.ui.details
 
+import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
@@ -11,6 +12,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import java.time.LocalTime
@@ -40,14 +43,29 @@ fun Clock(
     val offset = Offset(clockRadius, clockRadius)
     val angleOffset = 3/2f
 
-    val partialHourAngle = ((time.minute / 60f) / 12f * PI.toFloat())
-    val hoursAngle = (time.hour / 6f + angleOffset) * PI.toFloat() + partialHourAngle
     val hoursLengthMult = 0.4f
     val hoursWidth = 10f
 
-    val minutesAngle = (time.minute / 30f + angleOffset) * PI.toFloat()
     val minutesLengthMult = 0.6f
     val minutesWidth = 10f
+
+    val numbersPosMult = 0.7f
+
+    fun hPartialAngle(min: Int): Float {
+        return ((time.minute / 60f) / 6f * PI.toFloat())
+    }
+
+    fun hTruncatedAngle(hour: Int): Float {
+        return (hour / 6f + angleOffset) * PI.toFloat()
+    }
+
+    fun hAngle(hour: Int, min: Int): Float {
+        return hTruncatedAngle(hour) + hPartialAngle(min)
+    }
+
+    fun mAngle(min: Int): Float {
+        return (min / 30f + angleOffset) * PI.toFloat()
+    }
 
     Canvas(modifier
             .height(256.dp)
@@ -57,12 +75,16 @@ fun Clock(
         drawCircle(color = Color(0xFF262626), radius = clockRadius, center = offset)
         drawCircle(color = Color(0xFFFFFFFF), radius = 0.8f * clockRadius, center = offset)
 
+        // draw numbers
+        drawIntoCanvas { canvas ->
+            for (x in 1 until 13)
+                canvas.nativeCanvas.drawText("${x}", offset.x + numbersPosMult * clockRadius * cos(hTruncatedAngle(x)), offset.y + numbersPosMult * clockRadius * sin(hTruncatedAngle(x)), Paint())
+        }
         // Minutes hand
-        drawLine(color = Color(0xFFC22D2D), start = offset, end = offset.plus(Offset(cos(minutesAngle), sin(minutesAngle)).times(minutesLengthMult * clockRadius)), strokeWidth = minutesWidth)
+        drawLine(color = Color(0xFFC22D2D), start = offset, end = offset.plus(Offset(cos(mAngle(time.minute)), sin(mAngle(time.minute))).times(minutesLengthMult * clockRadius)), strokeWidth = minutesWidth)
 
         // Hours hand
-        drawLine(color = Color(0xFF262626), start = offset, end = offset.plus(Offset(cos(hoursAngle), sin(hoursAngle)).times(hoursLengthMult * clockRadius)), strokeWidth = hoursWidth)
-
+        drawLine(color = Color(0xFF262626), start = offset, end = offset.plus(Offset(cos(hAngle(time.hour, time.minute)), sin(hAngle(time.hour, time.minute))).times(hoursLengthMult * clockRadius)), strokeWidth = hoursWidth)
 
         // Center dot
         drawCircle(color = Color(0xFF262626), radius = 0.075f * clockRadius, center = offset)
